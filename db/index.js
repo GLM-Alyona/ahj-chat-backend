79d14198033b0417bc45a58af3d378f27fb575c9
Alyona <glmalyona@gmail.com>
2023-09-25 15:55:58 +0300

    2 commit

--- db/index.js
+++ db/index.js
@@ -1,27 +1,27 @@
 const subscriptions = {
-    data: [],
-    listeners: [],
-    sendObj: {},
-    
-    add(item) {
-      this.data.push(item);
-      this.sendObj.nickname = item.nickname;
-      this.sendObj.req = 'add'
+  data: [],
+  listeners: [],
+  sendObj: {},
   
-      this.listeners.forEach(handler => handler(this.sendObj));
-    },
+  add(item) {
+    this.data.push(item);
+    this.sendObj.nickname = item.nickname;
+    this.sendObj.req = 'add'
+
+    this.listeners.forEach(handler => handler(this.sendObj));
+  },
+
+  remove(item) {
+    this.data = this.data.filter(sub => {return sub.nickname !== item});
+    this.sendObj.nickname = item;
+    this.sendObj.req = 'remove';
+
+    this.listeners.forEach(handler => handler(this.sendObj));
+  },
   
-    remove(item) {
-      this.data = this.data.filter(sub => {return sub.nickname !== item});
-      this.sendObj.nickname = item;
-      this.sendObj.req = 'remove';
-  
-      this.listeners.forEach(handler => handler(this.sendObj));
-    },
-    
-    listen(handler) {
-      this.listeners.push(handler);
-    },
-  }
-  
-  module.exports = subscriptions;
+  listen(handler) {
+    this.listeners.push(handler);
+  },
+}
+
+module.exports = subscriptions;
